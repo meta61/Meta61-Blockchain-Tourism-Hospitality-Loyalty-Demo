60984f516446d5bfe59e562095d6305fb43301d1
Educated Squire <56125716+EducatedSquire9@users.noreply.github.com>
2024-05-14 18:07:07 +1000

    Update login-test.php

--- Test/login-test.php
+++ Test/login-test.php
@@ -153,7 +153,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo (login-test.php); ?>" method="post">
+        <form action="<?php echo htmlspecialchars(login-test.php); ?>" method="post">
             <div class="<?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
