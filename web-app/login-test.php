e789c9c69a5ec5354bc54f15d72426ef9d063656
Educated Squire <56125716+EducatedSquire9@users.noreply.github.com>
2024-05-14 17:02:01 +1000

    Update login-test.php

--- web-app/login-test.php
+++ web-app/login-test.php
@@ -1,96 +1,96 @@
-// <?php
+<?php
 // session_start();
 
-// // Check if the user is already logged in, redirect to home page if true
-// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-//     header("location: home.php");
-//     exit;
-// }
+// Check if the user is already logged in, redirect to home page if true
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: home.php");
+    exit;
+}
 
-// // Include config file
-// require_once "config.php";
-// // Include aoi configuration
-// require_once "log.php";
+// Include config file
+require_once "config.php";
+// Include aoi configuration
+require_once "log.php";
 
-// // Define variables and initialize with empty values
-// $username = $password = "";
-// $username_err = $password_err = $login_err = "";
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
 
-// // Processing form data when form is submitted
-// if($_SERVER["REQUEST_METHOD"] == "POST"){
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-//     // Check if username is empty
-//     if(empty(trim($_POST["username"]))){
-//         $username_err = "Please enter username.";
-//     } else{
-//         $username = trim($_POST["username"]);
-//     }
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
 
-//     // Check if password is empty
-//     if(empty(trim($_POST["password"]))){
-//         $password_err = "Please enter your password.";
-//     } else{
-//         $password = trim($_POST["password"]);
-//     }
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
 
-//     // Validate credentials
-//     if(empty($username_err) && empty($password_err)){
-//         // Prepare a select statement
-//         $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
 
-//         if($stmt = $mysqli->prepare($sql)){
-//             // Bind variables to the prepared statement as parameters
-//             $stmt->bind_param("s", $param_username);
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $param_username);
 
-//             // Set parameters
-//             $param_username = $username;
+            // Set parameters
+            $param_username = $username;
 
-//             // Attempt to execute the prepared statement
-//             if($stmt->execute()){
-//                 // Store result
-//                 $stmt->store_result();
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Store result
+                $stmt->store_result();
 
-//                 // Check if username exists, if yes then verify password
-//                 if($stmt->num_rows == 1){
-//                     // Bind result variables
-//                     $stmt->bind_result($id, $username, $hashed_password);
-//                     if($stmt->fetch()){
-//                         if(password_verify($password, $hashed_password)){
-//                             // Password is correct, so start a new session
-//                             //session_start();
+                // Check if username exists, if yes then verify password
+                if($stmt->num_rows == 1){
+                    // Bind result variables
+                    $stmt->bind_result($id, $username, $hashed_password);
+                    if($stmt->fetch()){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            //session_start();
 
-//                             // Store data in session variables
-//                             $_SESSION["loggedin"] = true;
-//                             $_SESSION["id"] = $id;
-//                             $_SESSION["username"] = $username;
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
 
-//                             //API Login
-//                             $_SESSION['registerResponse'] = registerUser($username, "Org1", "Hotel");
+                            //API Login
+                            $_SESSION['registerResponse'] = registerUser($username, "Org1", "Hotel");
 
-//                             // Redirect user to home page
-//                             header("location: home.php");
-//                         } else{
-//                             // Password is not valid, display a generic error message
-//                             $login_err = "Invalid username or password.";
-//                         }
-//                     }
-//                 } else{
-//                     // Username doesn't exist, display a generic error message
-//                     $login_err = "Invalid username or password.";
-//                 }
-//             } else{
-//                 echo "Oops! Something went wrong. Please try again later.";
-//             }
+                            // Redirect user to home page
+                            header("location: home.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
-//             // Close statement
-//             $stmt->close();
-//         }
-//     }
+            // Close statement
+            $stmt->close();
+        }
+    }
 
-//     // Close connection
-//     $mysqli->close();
-// }
-// ?>
+    // Close connection
+    $mysqli->close();
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -153,7 +153,7 @@
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <div class="<?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
